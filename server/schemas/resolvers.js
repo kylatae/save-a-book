2b15b870000e8830412b1854132ebf06954b2312
Arthur Henningfield <144608814+kylatae@users.noreply.github.com>
2023-12-03 17:55:26 -0600

    Updated resolvers and typeDefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,78 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+
+      throw AuthenticationError;
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    // Add a third argument to the resolver to access data in our `context`
+    saveBook: async (parent, { author, description, title, bookId, image, link}, context) => {
+      // If context has a `user` property, that means the user executing this mutation has a valid JWT and is logged in
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: userId },
+          {
+            $addToSet: { savedBooks: [{
+              author: author,
+              description: description,
+              title: title,
+              bookId: bookId,
+              image: image,
+              link: link
+          }]},
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      // If user attempts to execute this mutation and isn't logged in, throw an error
+      throw AuthenticationError;
+    },
+    // Make it so a logged in user can only remove a skill from their own user
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: [{bookId: bookId}]  } },
+          { new: true }
+        );
+      }
+      throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Number
+    savedBooks: [Book]
+  }
+  
+  type Book{
+    bookId: String
+    authors: [String]!
+    description String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(author: [String]!, description: String, title: String, bookId: String, image: String, link: String): User
+    removeBook: (bookId: String): User
+  }
+`;
+
+module.exports = typeDefs;

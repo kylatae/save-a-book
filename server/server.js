94c86f999b99e5d1854d47587472b0773fad4903
Arthur Henningfield <144608814+kylatae@users.noreply.github.com>
2023-12-04 05:29:31 -0600

    Another Server Fix Attempt

--- server/server.js
+++ server/server.js
@@ -27,11 +27,11 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../build/dist')));
 
-    // app.get('*', (req, res) => {
-    //   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-    // });
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
   }
 
   db.once('open', () => {
